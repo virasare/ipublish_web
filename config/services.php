bb025a0f0146cda31d7f6dcd8fc30b4748870266
virasare <virasare@gmail.com>
2024-10-23 13:22:38 +0800

    first commit

--- config/services.php
+++ config/services.php
@@ -35,12 +35,12 @@ return [
         ],
     ],
     
-    'google' => [
-        'driver' => 'google',
-        'client_id' => env('GOOGLE_DRIVE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
-        'refresh_token' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
-        'folder_id' => env('GOOGLE_DRIVE_FOLDER_ID'),
-        ],
+    // 'google' => [
+    //     'driver' => 'google',
+    //     'client_id' => env('GOOGLE_DRIVE_CLIENT_ID'),
+    //     'client_secret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+    //     'refresh_token' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+    //     'folder_id' => env('GOOGLE_DRIVE_FOLDER_ID'),
+    //     ],
 
 ];
